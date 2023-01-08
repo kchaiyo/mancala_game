6a60c9d10d083dd81cdcb76b34878b8318217e5f
kchaiyo <9104050+kchaiyo@users.noreply.github.com>
2023-01-08 17:13:48 +0700

    board state reflected on to the UI

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from kivy.app import App
+from kivy.logger import Logger
+from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ObjectProperty, StringProperty
 
 from sampleAgent import SampleAgent
 
@@ -82,6 +85,22 @@ def perceive_board(board_state, player):
 
 
 class MancalaBoard(Widget):
+    # Label values to show
+    pit_A1 = NumericProperty(0)
+    pit_A2 = NumericProperty(0)
+    pit_A3 = NumericProperty(0)
+    pit_A4 = NumericProperty(0)
+    pit_A5 = NumericProperty(0)
+    pit_A6 = NumericProperty(0)
+    pit_AS = NumericProperty(0)
+    pit_B1 = NumericProperty(0)
+    pit_B2 = NumericProperty(0)
+    pit_B3 = NumericProperty(0)
+    pit_B4 = NumericProperty(0)
+    pit_B5 = NumericProperty(0)
+    pit_B6 = NumericProperty(0)
+    pit_BS = NumericProperty(0)
+
     def __init__(self, **kwargs):
         super(MancalaBoard, self).__init__(**kwargs)
         # initial game's state
@@ -115,6 +134,7 @@ class MancalaBoard(Widget):
         # print(self._board_state)
         # print(self._player_turn)
         # print(self._stone_in_hand)
+        self.update_pit_stones()
         if self._is_turn_processing:
             if self._stone_in_hand == 0:
                 # No more stone in hand, proceed to the next turn
@@ -144,11 +164,28 @@ class MancalaBoard(Widget):
                 self._board_state[self._current_pit] += 1
                 self._stone_in_hand -= 1
 
+    def update_pit_stones(self):
+        self.pit_A1 = self._board_state['A1']
+        self.pit_A2 = self._board_state['A2']
+        self.pit_A3 = self._board_state['A3']
+        self.pit_A4 = self._board_state['A4']
+        self.pit_A5 = self._board_state['A5']
+        self.pit_A6 = self._board_state['A6']
+        self.pit_AS = self._board_state['AS']
+        self.pit_B1 = self._board_state['B1']
+        self.pit_B2 = self._board_state['B2']
+        self.pit_B3 = self._board_state['B3']
+        self.pit_B4 = self._board_state['B4']
+        self.pit_B5 = self._board_state['B5']
+        self.pit_B6 = self._board_state['B6']
+        self.pit_BS = self._board_state['BS']
+
 
 class MancalaApp(App):
     def __init__(self, **kwargs):
         super(MancalaApp, self).__init__(**kwargs)
         Window.size = (900, 600)
+        Builder.load_file('mancala.kv')
         self.mancalaBoard = MancalaBoard()
 
     def build(self):
